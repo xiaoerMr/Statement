2aa6795f2d7c8c05ba08a8f3fc93802eb5591d48
MCU <dongjw@junlelian.cn>
2023-04-11 14:51:57 +0800

    调整引用

--- buildSrc/src/main/kotlin/AndroidX.kt
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -5,10 +5,16 @@ object AndroidX {
     const val material = "com.google.android.material:material:1.6.1"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
 
-    const val lifecycleViewModelKtx= "2.5.1"
-    const val lifecycleRuntimeKtx= "2.5.1"
+    // 扩展
+    private const val vLifecycleViewModel= "2.5.1"
+    const val lifecycleRuntimeKtx= "androidx.lifecycle:lifecycle-runtime-ktx:$vLifecycleViewModel"
+    const val lifecycleViewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$vLifecycleViewModel"
+
+    // 协程
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
 
     const val junit = "junit:junit:4.13.2"
     const val junitExt = "androidx.test.ext:junit:1.1.3"
     const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+
 }
--- buildSrc/src/main/kotlin/OtherPart.kt
+++ buildSrc/src/main/kotlin/OtherPart.kt
@@ -1,6 +1,37 @@
 object OtherPart {
-    const val okhttp ="4.10.0"
-    const val okhttpLoggingInterceptor ="4.10.0"
-    const val retrofit ="2.9.0"
-    const val retrofitConverterGson ="2.9.0"
+
+    private const val vOkhttp = "4.10.0"
+    private const val vRetrofit = "2.9.0"
+    const val okhttp ="com.squareup.okhttp3:okhttp:$vOkhttp"
+    const val okhttpLoggingInterceptor ="com.squareup.okhttp3:logging-interceptor:$vOkhttp"
+    const val retrofit ="com.squareup.retrofit2:retrofit:$vRetrofit"
+    const val retrofitConverterGson ="com.squareup.retrofit2:converter-gson:$vRetrofit"
+//    implementation 'com.squareup.retrofit2:adapter-rxjava3:$vRetrofit'
+
+    // 适配
+    const val androidAutoSize = "com.github.JessYanCoding:AndroidAutoSize:v1.2.1"
+    // dialog
+    const val DialogX =  "com.kongzue.dialogx:DialogX:0.0.47"
+
+    // 基础依赖包，必须要依赖
+    private const val vImmersionBar = "3.2.2"
+    const val immersionBar =   "com.geyifeng.immersionbar:immersionbar:$vImmersionBar"
+    // kotlin扩展（可选）
+    const val immersionBarKtx =   "com.geyifeng.immersionbar:immersionbar-ktx:$vImmersionBar"
+    // fragment快速实现（可选）已废弃
+//    implementation "com.geyifeng.immersionbar:immersionbar-components:$vImmersionBar"
+
+    // adapt
+    const val baseRecyclerViewAdapterHelper =  "io.github.cymchad:BaseRecyclerViewAdapterHelper:4.0.0-beta02"
+
+    //    implementation("io.coil-kt:coil:2.2.2")
+    private const val vGlide = "4.15.0"
+    const val glide = "com.github.bumptech.glide:glide:$vGlide"
+    const val glideCompiler =  "com.github.bumptech.glide:compiler:$vGlide"
+
+    // 图片选择
+    private const val vPictureSelector = "v3.10.8"
+    const val pictureSelector = "io.github.lucksiege:pictureselector:$vPictureSelector"
+    const val pictureSelectorCompress = "io.github.lucksiege:compress:$vPictureSelector"
+    const val pictureSelectorUcrop = "io.github.lucksiege:ucrop:$vPictureSelector"
 }
