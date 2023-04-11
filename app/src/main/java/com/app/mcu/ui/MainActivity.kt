5398c8614b5f9de439ff1addaa03d067e5fb4ece
MCU <dongjw@junlelian.cn>
2023-04-11 15:49:21 +0800

    图片选择跳转

--- app/src/main/java/com/app/mcu/ui/MainActivity.kt
+++ app/src/main/java/com/app/mcu/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.app.mcu.ui
 
+import android.content.Intent
 import com.app.lib_comment.base.BaseVMActivity
+import com.app.mcu.PictureSelectorActivity
 import com.app.mcu.databinding.ActivityMainBinding
 import com.app.mcu.ui.vm.MainViewModel
 
@@ -17,6 +19,10 @@ class MainActivity : BaseVMActivity<ActivityMainBinding, MainViewModel>() {
     override fun initListener() {
         mViewBinding.vGoHttp.setOnClickListener {
             mViewModel.getNewList()
+        }
+
+        mViewBinding.vGoSelectPhone.setOnClickListener {
+            startActivity(Intent(this,PictureSelectorActivity::class.java))
         }
     }
 
